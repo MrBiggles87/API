ecb47bbcf5b3b24495cccbc7f052d6f9e3888a39
MrBiggles87 <garry.adams@nationwide.co.uk>
2019-06-27 14:48:58 +0100

    Updated GET, POST and DELETE in NoteController

--- qa/com/controller/NoteController.java
+++ qa/com/controller/NoteController.java
@@ -3,6 +3,8 @@ package qa.com.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -20,5 +22,22 @@ public class NoteController {
 	@RequestMapping(value = "notes", method = RequestMethod.GET)
 	public List<Note> list(){
 		return NotesRepository.findAll();
+	}
+	
+	@RequestMapping(value = "notes", method = RequestMethod.POST)
+	public Note create(@RequestBody Note note) {
+		return NotesRepository.saveAndFlush(note);
+	}
+	
+	@RequestMapping(value = "notes/{id}", method = RequestMethod.GET)
+	public Note getNote(@PathVariable Long id) {
+		return NotesRepository.findOne(id);
+	}
+	
+	@RequestMapping(value = "notes/{id}", method = RequestMethod.DELETE)
+	public Note delete(@PathVariable Long id) {
+		Note existingNote = NotesRepository.findOne(id);
+		NotesRepository.delete(existingNote);
+		return existingNote;
 	}
 }
