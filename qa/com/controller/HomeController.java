e6f47266bf00c79c75d79ae49af7f4bb7826318e
MrBiggles87 <garry.adams@nationwide.co.uk>
2019-06-27 11:26:38 +0100

    Added a method for the homepage

--- qa/com/controller/HomeController.java
+++ qa/com/controller/HomeController.java
@@ -1,8 +1,14 @@
 package qa.com.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+	
+	@RequestMapping("/") //This annotation is for specifying the url that will trigger this method.
+	public String home() {
+		return "Hello Everyone";
+	}
 
 }
